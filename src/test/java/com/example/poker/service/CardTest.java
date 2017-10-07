4464843898b957839a7a75e2a461cd900041c3de
Mauro Storch <maurostorch@gmail.com>
2017-10-07 16:51:46 -0400

    Card test coverage

---
+++ src/test/java/com/example/poker/service/CardTest.java
@@ -0,0 +1,49 @@
+package com.example.poker.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CardTest {
+
+	@Test
+	public void testCardConstructorForSuit() {
+		Card card = new Card("hearts","ace");
+		
+		assertNotEquals("ace", card.getSuit());
+		assertEquals("hearts", card.getSuit());
+		
+	}
+	
+	@Test
+	public void testCardConstructorForValue() {
+		Card card = new Card("hearts","ace");
+		
+		assertEquals("ace", card.getValue());
+		assertNotEquals("hearts", card.getValue());
+	}
+	
+	@Test
+	public void testEquals() {
+		Card c1 = new Card("hearts","ace");
+		Card c2 = new Card("hearts","2");
+		Card c3 = new Card("clubs","ace");
+		Card c4 = new Card("clubs","2");
+		Card c5 = new Card("hearts","ace");
+		
+		assertFalse(c1.equals(null));
+		assertTrue(c1.equals(c1));
+		assertFalse(c1.equals(c2));
+		assertFalse(c1.equals(c3));
+		assertFalse(c1.equals(c4));
+		assertTrue(c1.equals(c5));		
+		
+	}
+	
+	public void testToString() {
+		Card c1 = new Card("hearts","ace");
+		
+		assertEquals("{\"suit\":\"hearts\",\"value\":\"ace\"}", c1.toString());
+	}
+	
+}
