e5e2404e4be4535836f5ba1bcc97ff492275b5c0
Mauro Storch <maurostorch@gmail.com>
2017-10-08 12:50:31 -0400

    LinkedList for using remove element from Iterator

--- src/main/java/com/example/poker/service/MemoryCardsService.java
+++ src/main/java/com/example/poker/service/MemoryCardsService.java
@@ -2,11 +2,13 @@ package com.example.poker.service;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
+import java.util.logging.Logger;
 
 public class MemoryCardsService implements CardsService {
+	
+	public final static Logger log = Logger.getLogger(MemoryCardsService.class.getName());
 
 	private final static String suits[] = {"hearts", "spades", "clubs", "diamonds"};
 	private final static String values[] = {"Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"};
@@ -14,42 +16,44 @@ public class MemoryCardsService implements CardsService {
 	private List<Card> out;
 	
 	public MemoryCardsService() {
-		// This is a LinkedList since adding and removing elements in the middle during shuffle is O(1)
-		this.deck = new LinkedList<>();
+		this.deck = new ArrayList<>();
 		for (String s:Arrays.asList(suits)) {
 			for (String v:Arrays.asList(values)) {
 				this.deck.add(new Card(s,v));
 			}
 		}
-		// This is an ArrayList since is used only to store
 		this.out = new ArrayList<>();
 	}
 	
 	@Override
 	public Card getCardFromTop() throws Exception {
+		log.fine("Dealing one card.");
 		try {
-			Card c = this.deck.remove(0);
+			Card c = this.deck.remove(this.deck.size()-1);
 			out.add(c);
 			return c;
 		} catch (IndexOutOfBoundsException e) {
+			log.fine("No more cards");
 			throw new Exception("No more cards");
 		}
 	}
 
 	@Override
 	public void shuffleCards() {
+		log.fine("Shuffling..");
 		Random r = new Random(System.currentTimeMillis());
 		this.reset();
 		int size = this.deck.size();
-		for (int i = 0;i<size;i++) {
-			Card _t = this.deck.remove(r.nextInt(size));
-			this.deck.add(r.nextInt(size), _t);
+		for (int i = size-1;i>0;i--) {
+			int random = r.nextInt(i+1);
+			Card _t = this.deck.get(i);
+			this.deck.set(i, this.deck.get(random));
+			this.deck.set(random, _t);
 		}
+		log.fine("Shuffling.. ended.");
 	}
 
 	private void reset() {
-		this.out.addAll(deck);
-		this.deck.clear();
 		this.deck.addAll(this.out);
 		this.out.clear();
 	}
--- src/main/java/com/example/poker/service/MemoryOptimizedCardsService.java
+++ src/main/java/com/example/poker/service/MemoryOptimizedCardsService.java
@@ -2,11 +2,16 @@ package com.example.poker.service;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Random;
+import java.util.logging.Logger;
 
 public class MemoryOptimizedCardsService implements CardsService {
+	
+	public final static Logger log = Logger.getLogger(MemoryOptimizedCardsService.class.getName());
 
 	private final static String suits[] = {"hearts", "spades", "clubs", "diamonds"};
 	private final static String values[] = {"Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"};
@@ -14,41 +19,44 @@ public class MemoryOptimizedCardsService implements CardsService {
 	private List<Card> out;
 	
 	public MemoryOptimizedCardsService() {
-		// This is a LinkedList since adding and removing elements in the middle during shuffle is O(1)
 		this.deck = new LinkedList<>();
 		for (String s:Arrays.asList(suits)) {
 			for (String v:Arrays.asList(values)) {
 				this.deck.add(new Card(s,v));
 			}
 		}
-		// This is an ArrayList since is used only to store
 		this.out = new ArrayList<>();
 	}
 	
 	/**
 	 * The optimization is related to avoiding handling the entire list
 	 * during the shuffle and enjoy the O(1) to remove from LinkedList
+	 * when using an Iterator and its remove method
 	 */
 	@Override
 	public Card getCardFromTop() throws Exception {
+		log.fine("Dealing one card.");
 		try {
 			int next = new Random(System.currentTimeMillis()).nextInt(this.deck.size()==0?1:this.deck.size());
-			Card c = this.deck.remove(next);
+			Iterator<Card> iter = this.deck.listIterator(next);
+			Card c = iter.next();
+			iter.remove();
 			out.add(c);
 			return c;
-		} catch (IndexOutOfBoundsException e) {
+		} catch (NoSuchElementException e) {
+			log.fine("No more cards");
 			throw new Exception("No more cards");
 		}
 	}
 
 	@Override
 	public void shuffleCards() {
+		log.fine("Shuffling..");
 		this.reset();
+		log.fine("Shuffling.. ended.");
 	}
 
 	private void reset() {
-		this.out.addAll(deck);
-		this.deck.clear();
 		this.deck.addAll(this.out);
 		this.out.clear();
 	}
